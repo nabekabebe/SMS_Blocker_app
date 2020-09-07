cf14348951678e9fbca0b5e68e1c759866f2c6f5
nabek <nabekabebe6@gmail.com>
2020-09-08 00:16:27 +0300

    Navigation drawer added. two tabs implemented with empty recycler view to list all sms.

--- app/src/main/java/com/example/smsblocker/MainActivity.java
+++ app/src/main/java/com/example/smsblocker/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.smsblocker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
     }
+
 }
